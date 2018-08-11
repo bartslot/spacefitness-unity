aea0ba70edf4bd98aa31ebf723a04cd8cda0143a
bartslot <bartslot@gmail.om>
2018-08-11 14:08:42 +0200

    v1.2
    Added more Sci-fi audio to assets

---
+++ Assets/Assets/_Scripts/GameController.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text scoreText;
+    public Text restartText;
+    public Text gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+    public Camera firstPersonCamera;
+    public Camera MainCamera;
+
+    
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+    }
+
+    void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            // Application.LoadLevel(Application.loadedLevel);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (Input.GetKeyDown(KeyCode.C)) {
+            if (MainCamera.enabled == false)
+            {
+                ShowOverheadView();
+                return;
+            }
+            ShowFirstPersonView();
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+    public void ShowOverheadView() {
+        firstPersonCamera.enabled = false;
+        MainCamera.enabled = true;
+    }
+    
+    public void ShowFirstPersonView() {
+        firstPersonCamera.enabled = true;
+        MainCamera.enabled = false;
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+}
---
+++ Assets/Assets/_Scripts/movingBackground.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingBackground : MonoBehaviour {
+    // SETTING FOR THE SCROLLSPEED OF THE MOVING BACKGROUND
+    public float scrollSpeed = 0.5F;
+    public Renderer rend;
+    void Start() {
+        rend = GetComponent<Renderer>();
+    }
+    // MAKING THE BACKGROUND SCROLL
+    void Update() {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+    }
+}
